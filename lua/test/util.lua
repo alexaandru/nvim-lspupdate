8270b2e8c19e46e6c510b6a07f2a6e3ab6013647
Alex Ungur <alexaandru@gmail.com>
2021-01-29 16:16:23 +0200

    Made the testing code more reusable

--- lua/test/util.lua
+++ lua/test/util.lua
@@ -1,13 +1,6 @@
--- adhoc, micro testing framework:
--- keys represnt functions to run in the target package (being tested)
--- values are lists of a "testCase", where each testCase is a
--- map with `args` (table, will be unpacked into actual arguments)
--- and `exp` - the expectation, will be used as is.
---
--- Run it with: `neovim --headless +'luafile lua/lspupdate/util_test.lua' +q`
--- no error/output == success!
 local util = require "lspupdate/util"
-local testCases = {
+
+util.test(util, {
   lspVal = {
     {args = {""}, exp = {""}},
     {args = {"foo|"}, exp = {""}},
@@ -22,14 +15,4 @@ local testCases = {
     {args = {{"foo", {"bar", "baz"}}, " "}, exp = "foo bar baz"},
   },
   osCapture = {{args = {"echo hello world"}, exp = "hello world"}},
-}
-
-local I = vim.inspect
-for k, v in pairs(testCases) do
-  for _, tc in pairs(v) do
-    local act = util[k](unpack(tc.args))
-    assert(vim.deep_equal(tc.exp, act),
-           "testing util." .. k .. "(), expected " .. I(tc.exp) .. " got "
-               .. I(act) .. " for args " .. I(tc.args))
-  end
-end
+})
